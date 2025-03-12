65aa64a5f5c20f4d2bf6d4367750ec79df02e1ed
Endy1381 <119288346+Endy1381@users.noreply.github.com>
2025-03-12 05:48:31 +0300

    Init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./comp/header";
+import Footer from "./comp/footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
